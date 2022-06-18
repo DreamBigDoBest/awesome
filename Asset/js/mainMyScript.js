c253f24befb9943d24aa10475cde99ca296f85b0
DreamBigDoBest <106167404+DreamBigDoBest@users.noreply.github.com>
2022-06-18 17:08:55 +0800

    Add files via upload

---
+++ Asset/js/mainMyScript.js
@@ -0,0 +1,52 @@
+
+function mediaSelection(index)
+{
+    switch (index)
+    {
+        case 1:
+            console.log("selected-1");
+            localStorage.setItem("login-token", "selected-1");
+            window.location.assign("player.html");
+            break;
+        case 2:
+            console.log("selected-2");
+            localStorage.setItem("login-token", "selected-2");
+            window.location.assign("player.html");
+            break;
+        default:
+            break;
+    }
+}
+
+
+
+function backGroundProcess()
+{
+    var hashChecker = 37047011;
+    if(hashCode(window.location.href) != hashChecker)
+    {
+        window.location.replace("index.html");
+        return;
+    }
+    
+    if(localStorage.getItem("login-token") == "28021990")
+    {
+        document.getElementById("selection1").addEventListener("click", function(){mediaSelection(1);});
+        document.getElementById("selection2").addEventListener("click", function(){mediaSelection(2);});
+    }
+    else
+    {
+        window.location.replace("index.html");
+        return;
+    }
+}
+
+function hashCode(str) {
+    let hash = 0;
+    for (let i = 0, len = str.length; i < len; i++) {
+        let chr = str.charCodeAt(i);
+        hash = (hash << 5) - hash + chr;
+        hash |= 0; // Convert to 32bit integer
+    }
+    return hash;
+}
